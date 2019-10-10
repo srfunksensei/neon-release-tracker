80261118dc6ecb400a42989e31c474ea444453b6
MB <sr.funk.sensei@gmail.com>
2019-10-10 20:09:25 +0200

    Initial commit

---
+++ src/main/java/com/mb/neonreleasetracker/NeonReleaseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mb.neonreleasetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonReleaseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeonReleaseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mb/neonreleasetracker/NeonReleaseTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mb.neonreleasetracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeonReleaseTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
