dc638d6d558c9f779002b7e3bd1a798e7889bee0
MB <sr.funk.sensei@gmail.com>
2019-10-12 21:08:39 +0200

    added controller with tests

---
+++ src/main/java/com/mb/neonreleasetracker/controller/ReleaseController.java
@@ -0,0 +1,86 @@
+package com.mb.neonreleasetracker.controller;
+
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mb.neonreleasetracker.assembler.resource.release.ReleaseResource;
+import com.mb.neonreleasetracker.assembler.resource.release.ReleaseResourceAssemblerSupport;
+import com.mb.neonreleasetracker.dto.ReleaseDto;
+import com.mb.neonreleasetracker.model.Release;
+import com.mb.neonreleasetracker.service.ReleaseService;
+
+@RestController
+@RequestMapping(value = "/releases", produces = "application/hal+json")
+public class ReleaseController {
+	
+	private ReleaseService releaseService;
+	
+	private ReleaseResourceAssemblerSupport releaseResourceAssembler;
+	private PagedResourcesAssembler<Release> pagedAssembler;
+	
+	@Autowired
+	public ReleaseController(final ReleaseService releaseService, 
+			final ReleaseResourceAssemblerSupport releaseResourceAssembler,
+			final PagedResourcesAssembler<Release> pagedAssembler) {
+		this.releaseService = releaseService;
+		this.releaseResourceAssembler = releaseResourceAssembler;
+		this.pagedAssembler = pagedAssembler;
+	}
+
+	@GetMapping
+	public HttpEntity<PagedResources<ReleaseResource>> findAll( //
+			@RequestParam(value = "search", required = false) String search, //
+			Pageable pageable) {
+		final Page<Release> releases = releaseService.findAll(search, pageable);
+		return new ResponseEntity<>(pagedAssembler.toResource(releases, releaseResourceAssembler), HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{releaseId}")
+	public ResponseEntity<ReleaseResource> findOne(@PathVariable Long releaseId) {
+		final Optional<ReleaseResource> resourceOpt = releaseService.findOne(releaseId).map(releaseResourceAssembler::toResource);
+		return resourceOpt //
+				.map(ResponseEntity::ok) //
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping(value = "/create")
+	public ResponseEntity<ReleaseResource> create(@Valid @RequestBody ReleaseDto releaseDto) {
+		final Release release = releaseService.create(releaseDto);
+		return new ResponseEntity<>(releaseResourceAssembler.toResource(release), HttpStatus.CREATED);
+	}
+
+	@PutMapping(value = "/update")
+	public ResponseEntity<ReleaseResource> update(@Valid @RequestBody ReleaseDto releaseDto) {
+		final Optional<Release> releaseOpt = releaseService.update(releaseDto);
+		
+		if (!releaseOpt.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return new ResponseEntity<>(releaseResourceAssembler.toResource(releaseOpt.get()), HttpStatus.OK);
+	}
+	
+	@DeleteMapping(value = "/{releaseId}")
+	public void deleteOne(@PathVariable Long releaseId) {
+		releaseService.deleteOne(releaseId);
+	}
+}
---
+++ src/test/java/com/mb/neonreleasetracker/controller/ReleaseControllerTest.java
@@ -0,0 +1,214 @@
+package com.mb.neonreleasetracker.controller;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.config.HateoasAwareSpringDataWebConfiguration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.mb.neonreleasetracker.assembler.resource.release.ReleaseResourceAssemblerSupport;
+import com.mb.neonreleasetracker.dto.ReleaseDto;
+import com.mb.neonreleasetracker.model.Release;
+import com.mb.neonreleasetracker.model.ReleaseStatus;
+import com.mb.neonreleasetracker.service.ReleaseService;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ReleaseController.class)
+@Import(HateoasAwareSpringDataWebConfiguration.class)
+public class ReleaseControllerTest {
+
+	@TestConfiguration
+	static class ReleaseControllerTestContextConfiguration {
+		@Bean
+		public ReleaseResourceAssemblerSupport releaseResourceAssembler() {
+			return new ReleaseResourceAssemblerSupport();
+		}
+	}
+
+	@Autowired
+	private MockMvc mvc;
+
+	@MockBean
+	private ReleaseService releaseServiceMock;
+
+	@Test
+	public void givenAnyFilter_whenGetAllReleases_thenReturnEmptyJsonArray() throws Exception {
+		final Page<Release> noReleases = new PageImpl<>(new ArrayList<Release>());
+
+		when(releaseServiceMock.findAll(ArgumentMatchers.isNull(), any(Pageable.class))).thenReturn(noReleases);
+
+		mvc.perform(get("/releases") //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk()) //
+				.andExpect(jsonPath("$.page.totalElements", equalTo(0)));
+
+		verify(releaseServiceMock, times(1)).findAll(ArgumentMatchers.isNull(), any(Pageable.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenAnyFilter_whenGetAllReleases_thenReturnJsonArray() throws Exception {
+		final Release release1 = createRelease(1L), release2 = createRelease(2L);
+
+		final List<Release> releases = Collections.unmodifiableList(Arrays.asList(release1, release2));
+		final Page<Release> releasePages = new PageImpl<>(releases);
+
+		when(releaseServiceMock.findAll(ArgumentMatchers.isNull(), any(Pageable.class))).thenReturn(releasePages);
+
+		mvc.perform(get("/releases") //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk()) //
+				.andExpect(jsonPath("$.page.totalElements", equalTo(releases.size())));
+
+		verify(releaseServiceMock, times(1)).findAll(ArgumentMatchers.isNull(), any(Pageable.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenAnyFilter_whenGetAllReleases_thenReturnJsonArrayPaginatedSize() throws Exception {
+		final Release release1 = createRelease(1L), release2 = createRelease(2L);
+
+		final List<Release> releases = Collections.unmodifiableList(Arrays.asList(release1, release2));
+		final Page<Release> releasePages = new PageImpl<>(releases);
+
+		when(releaseServiceMock.findAll(ArgumentMatchers.isNull(), any(Pageable.class))).thenReturn(releasePages);
+
+		final int pageSize = 1;
+
+		mvc.perform(get("/releases") //
+				.param("size", "" + pageSize) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk()) //
+				.andExpect(jsonPath("$.page.totalElements", equalTo(releases.size()))) //
+				.andExpect(
+						jsonPath("$._embedded.releaseResourceList[0].releaseId", equalTo(release1.getId().intValue())));
+
+		verify(releaseServiceMock, times(1)).findAll(ArgumentMatchers.isNull(), any(Pageable.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenId_whenGetRelease_thenReturnOk() throws Exception {
+		final Release release = createRelease(1L);
+
+		when(releaseServiceMock.findOne(any(Long.class))).thenReturn(Optional.of(release));
+
+		mvc.perform(get("/releases/" + release.getId()) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk()) //
+				.andExpect(jsonPath("$.releaseId", equalTo(release.getId().intValue()))) //
+				.andExpect(jsonPath("$.title", equalTo(release.getTitle()))) //
+				.andExpect(jsonPath("$.description", equalTo(release.getDescription()))) //
+				.andExpect(jsonPath("$.status", equalTo(release.getStatus().toString())));
+
+		verify(releaseServiceMock, times(1)).findOne(any(Long.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenId_whenGetRelease_thenReturnNotFound() throws Exception {
+		when(releaseServiceMock.findOne(any(Long.class))).thenReturn(Optional.empty());
+
+		mvc.perform(get("/releases/1") //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isNotFound());
+
+		verify(releaseServiceMock, times(1)).findOne(any(Long.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenRelease_whenCreateRelease_thenReturnCreated() throws Exception {
+		final Release release = createRelease(1L);
+		when(releaseServiceMock.create(any(ReleaseDto.class))).thenReturn(release);
+
+		final String jsonDto = "{\"title\" : \"milan12\",\"description\" : \"d2\", \"status\": \""
+				+ ReleaseStatus.IN_DEVELOPMENT.toString() + "\"}";
+
+		mvc.perform(post("/releases/create") //
+				.content(jsonDto) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isCreated()) //
+				.andExpect(jsonPath("$.releaseId", equalTo(release.getId().intValue()))) //
+				.andExpect(jsonPath("$.title", equalTo(release.getTitle()))) //
+				.andExpect(jsonPath("$.description", equalTo(release.getDescription()))) //
+				.andExpect(jsonPath("$.status", equalTo(release.getStatus().toString())));
+
+		verify(releaseServiceMock, times(1)).create(any(ReleaseDto.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenRelease_whenUpdateRelease_thenReturnOk() throws Exception {
+		final Release release = createRelease(1L);
+		when(releaseServiceMock.update(Mockito.any(ReleaseDto.class))).thenReturn(Optional.of(release));
+
+		final String jsonDto = "{\"releaseId\" : " + release.getId()
+				+ ",\"title\" : \"milan12\",\"description\" : \"d2\", \"status\": \""
+				+ ReleaseStatus.IN_DEVELOPMENT.toString() + "\"}";
+		mvc.perform(put("/releases/update") //
+				.content(jsonDto) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk());
+
+		verify(releaseServiceMock, times(1)).update(any(ReleaseDto.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	@Test
+	public void givenRelease_whenUpdateRelease_thenReturnNotFound() throws Exception {
+		when(releaseServiceMock.update(Mockito.any(ReleaseDto.class))).thenReturn(Optional.empty());
+
+		final String jsonDto = "{\"title\" : \"milan12\",\"description\" : \"d2\"}";
+		mvc.perform(put("/releases/update") //
+				.content(jsonDto) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isNotFound());
+
+		verify(releaseServiceMock, times(1)).update(any(ReleaseDto.class));
+		verifyNoMoreInteractions(releaseServiceMock);
+	}
+
+	private Release createRelease(final Long id) {
+		final Release release = new Release();
+		release.setId(id);
+		release.setCreatedDate(LocalDate.now());
+		release.setUpdatedDate(LocalDate.now());
+		release.setTitle("title");
+		release.setDescription("description");
+		release.setStatus(ReleaseStatus.CREATED);
+		release.setReleaseDate(null);
+		return release;
+	}
+}
